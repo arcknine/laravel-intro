7181c664cd95f819c67291f6dfc02ca98662a3b9
Keenan Iban <keenan.iban@gmail.com>
2019-08-13 04:30:27 +0800

    added mail

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Weocome to MyInstagram
+
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam error aspernatur repellat cum commodi eaque, facilis delectus! Fuga temporibus commodi laborum inventore aliquid atque dolores libero nisi! Nam, repudiandae quia?
+
+Thanks,<br>
+MyInstagram Devs
+@endcomponent
